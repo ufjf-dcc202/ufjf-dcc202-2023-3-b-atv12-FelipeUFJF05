d447421a44b297fc5b6c416ab62762343f7d5a64
Felipe Lima Nogueira <felipe.nogueira@estudante.ufjf.br>
2023-12-05 22:39:26 -0300

    adiciona a função transação e dePomarParaPessoa

--- estoque.js
+++ estoque.js
@@ -14,4 +14,40 @@ function getEstoque(){
     return structuredClone(estoque);
 }
 
-export {getEstoque};
+function transacao(origem, destino, tipo, quantidade){
+    if(destino === "pomar")
+    {
+        const pessoa = estoque[origem];
+        for(let i = 0; i < pessoa.length; i++)
+        {
+            const monte = pessoa[i];
+            if(monte.tipo === tipo)
+            {
+                monte.qtd -= Math.min(quantidade, monte.qtd);
+                return;
+            }
+        }
+
+    }
+    if(origem === "pomar")
+    {
+        dePomarParaPessoa(destino, quantidade);
+    }
+}
+
+function dePomarParaPessoa(destino, quantidade){
+    const pessoa = estoque[destino];
+        for(let i = 0; i < pessoa.length; i++)
+        {
+            const monte = pessoa[i];
+            if(monte.tipo === tipo)
+            {
+                monte.qtd += Math.min(quantidade, 0);
+                return;
+            }
+        }
+        const novoMonte = {'tipo': tipo, 'qtd': Math.max(quantidade, 0)};
+        pessoa.push(novoMonte);
+}
+
+export {getEstoque, transacao};
